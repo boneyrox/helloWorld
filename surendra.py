b4d14ee4ed40d0c91e4165352c9d6085e6a0bf99
YAGNESH <yagnesh.patel9898@gmail.com>
2020-07-21 20:26:16 +0530

    Updated python file
    ADD a COLOR IN Circle

--- surendra.py
+++ surendra.py
@@ -5,10 +5,17 @@ t = turtle.Turtle()
   
 # radius of the circle 
 r = 40
-  
+t.begin_fill()
+
 # Loop for printing concentric circles 
 for i in range(4): 
     t.circle(r * i) 
     t.up() 
     t.sety((r * i)*(-1)) 
-    t.down() 
+    t.down()     
+
+col = 'red'    
+t.fillcolor(col) 
+t.end_fill() 
+
+ 
