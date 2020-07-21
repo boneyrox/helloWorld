d9881bf61bfb26540aa72197de15e3a7785e9073
Surendra9466 <46235720+Surendra9466@users.noreply.github.com>
2020-07-21 20:19:17 +0530

    Add files via upload

---
+++ surendra.py
@@ -0,0 +1,14 @@
+import turtle 
+    
+      
+t = turtle.Turtle() 
+  
+# radius of the circle 
+r = 40
+  
+# Loop for printing concentric circles 
+for i in range(4): 
+    t.circle(r * i) 
+    t.up() 
+    t.sety((r * i)*(-1)) 
+    t.down() 
